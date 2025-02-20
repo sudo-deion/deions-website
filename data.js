ed1f30ed937126addb4b57dc8370bf95f75cedff
Deion Simpson <deionjla@gmail.com>
2025-02-20 04:32:45 -0800

    data for projects in webflow

---
+++ data.js
@@ -0,0 +1,24 @@
+export const data = [
+    {
+      title: "Zuna Organic",
+      tagline: "Web Design",
+      tag: "UI, UX Design",
+      year: "2024",
+      link: "https://www.deion.design/projects/zuna",
+    },
+    {
+      title: "Fido Alliance",
+      tagline: "Web Design",
+      tag: "UI, UX Design",
+      year: "2023",
+      link: "https://www.deion.design/projects/fido-alliance",
+    },
+    {
+      title: "Gems",
+      tagline: "Mobile App",
+      tag: "UI, UX Design",
+      year: "2022",
+      link: "https://www.deion.design/projects/gems",
+    },
+  ];
+  
